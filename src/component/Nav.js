184d5ca78506bb60a3621dc68688c4a0112b12a4
Ibrahim Ahmed <ibra8imit07@gmail.com>
2024-11-01 01:14:29 +0300

    Change in Nav.js

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from "../images/Logo .svg";
 
 const Nav = () => {
-  return <nav>Nav page</nav>;
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggelMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav className={`navbar ${menuOpen ? "open" : ""} `}>
+      <a href="/" className="logo">
+        <img src={logo} alt="logo" />
+      </a>
+
+      <div className="menu-icon" onClick={toggelMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+      <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">Aboute</a>
+        </li>
+        <li>
+          <a href="/">Services</a>
+        </li>
+        <li>
+          <a href="/">Menu</a>
+        </li>
+        <li>
+          <a href="/">Reservation</a>
+        </li>
+        <li>
+          <a href="/">Order online</a>
+        </li>
+        <li>
+          <a href="/">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
