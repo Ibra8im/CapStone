21f8e30cae92d796da1b55e9b4326b952558d94e
Ibrahim Ahmed <ibra8imit07@gmail.com>
2024-11-01 01:56:07 +0300

    Change in Header.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Nav from "./component/Nav";
+import Header from "./component/Header";
 
 function App() {
   return (
     <>
       <Nav />
+      <Header />
     </>
   );
 }
---
+++ src/component/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import imageBanner from "../images/restauranfood.jpg";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <section>
+        {/* text */}
+        <div className="banner">
+          <h2>Littel Leamon</h2>
+          <h3>Chicago</h3>
+          <p>
+            We are a family Owned Mediteraneran Resturant, Focused on
+            traditional recipes served with a modern twist....
+          </p>
+          <Link to="/booking">
+            <button aria-label="on Click">Reserve table</button>{" "}
+          </Link>
+        </div>
+        {/* image */}
+        <div className="banner-img">
+          <img src={imageBanner} alt=""/>
+        </div>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
